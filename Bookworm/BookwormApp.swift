82e785db62b1deaee8a7ed89bc564c09534a2406
Joe Zych <zychjoe@gmail.com>
2023-01-12 18:28:50 -0500

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Joe Zedd on 1/12/23.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
